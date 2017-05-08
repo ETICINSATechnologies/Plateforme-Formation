19f2498d1538443d9d4d8e15ef0a58295c748d15
Anis Hamroun <anis_945@hotmail.fr>
2017-05-08 16:28:41 +0200

    liste pole -> liste formation

---
+++ formationCLIENT/jApp/components/listeformations/listeformations.component.js
@@ -0,0 +1,44 @@
+(function (module) {
+    module.component('listeformations',
+        {
+            restrict: 'E',
+            templateUrl: "jApp/components/listeformations/listeformations.template.html",
+            controller: [
+                function ()
+                {
+                    this.formation=[
+                        {
+                              name: 'DSI',
+                              description: "Système d'information"
+                            },
+                            {
+                              name: 'Performance',
+                              description: "Suivi et amélioration continue"
+
+                            }, 
+                            {
+                              name: "Unité d'affaire",
+                              description: "Vente et réalisation de missions"
+
+                            },
+                            {
+                              name: "Relation Client",
+                              description: "Prise de contact et fidélisation"
+
+                            },
+                            {
+                              name: "Communication",
+                              description: "Evénement et cohésion d'équipe"
+
+                            },
+                            {
+                              name: "Trésorerie",
+                              description: "Gestion des finances"
+
+                            }];
+                      }
+            ],
+            controllerAs: 'ListeFormationsCtrl'
+        }
+    );
+})(app);
